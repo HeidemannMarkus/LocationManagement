97acc82e8eb4e6b456d7826420485235309d2eb3
Markus Heidemann <heidemann@netzwerkplan.de>
2016-05-20 12:14:20 +0200

    implement LoadHardware implement UpdateHardware replace list of hardware with Dictionary

--- ProcessHardwareLocations/ProcessHardwareLocations.cs
+++ ProcessHardwareLocations/ProcessHardwareLocations.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ParserLibrary;
 using ProcessHardwareLocations.Data;
 
 namespace ProcessHardwareLocations
 {
     public class ProcessHardwareLocations : IProcessHardwareLocations
     {
-       private List<IHardware> HardWareList { get; set; }
+       private Dictionary<Guid, IHardware> HardWareList { get; set; }
        public IResult CaptureHardware(IHardware newHardware)
        {
-         var returnModel = new Result{};
+         var returnModel = new Result();
           if (newHardware.Id == Guid.Empty)
           {
              newHardware.Id = Guid.NewGuid();
@@ -19,7 +22,7 @@ namespace ProcessHardwareLocations
              returnModel.HasError = true;
              returnModel.ErrorMessage = "Id wird intern gesetzt! Feld muss Guid.Empty enthalten";
           }
-          HardWareList.Add(newHardware);
+         HardWareList.Add(newHardware.Id, newHardware);
 
           return returnModel;
        }
@@ -31,17 +34,47 @@ namespace ProcessHardwareLocations
 
        public List<IHardware> GetHardware()
        {
-          return HardWareList;
+          return HardWareList.Values.ToList();
        }
 
        public IResult LoadHardware(string filepath)
        {
-          throw new NotImplementedException();
+         var resultModel = new Result();
+         switch (Path.GetExtension(filepath))
+         {
+            case ".dat":
+               new BinaryParser().FromFile<IHardware>(filepath);
+               break;
+            case ".xml":
+               new XmlParser().FromFile<IHardware>(filepath);
+               break;
+            case ".json":
+               new JsonParser().FromFile<IHardware>(filepath);
+               break;
+            case ".csv":
+               new CsvParser().FromFile<IHardware>(filepath);
+               break;
+            default:
+               resultModel.HasError = true;
+               resultModel.ErrorMessage ="Format Unbekannt!!!!";
+               break;
+         }
+          return resultModel;
        }
 
        public IResult UpdateHardware(IHardware updatedHardware)
        {
-          throw new NotImplementedException();
+          var resultModel = new Result();
+         
+          if (HardWareList[updatedHardware.Id] == null)
+          {
+             resultModel.HasError = true;
+             resultModel.ErrorMessage = "hardware nicht gefunden";
+             return resultModel;
+          }
+
+          HardWareList[updatedHardware.Id] = updatedHardware;
+          return resultModel;
        }
 
        public IResult SaveHardware(string filepath)
