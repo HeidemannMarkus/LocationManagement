7c59ab71fb76748e3432521a4c85c5023e246626
Markus <Markus@GAMING_PC>
2016-05-22 20:18:23 +0200

    unfinished gui stuff

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -11,42 +11,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ProcessHardwareLocations;
+using ProcessHardwareLocations.Data;
+
 
 namespace WpfApplication1
 {
-   /// <summary>
-   /// Interaction logic for MainWindow.xaml
-   /// </summary>
-   public partial class MainWindow : Window
-   {
-       public MainWindow()
-       {
-           InitializeComponent();
-       }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IProcessHardwareLocations _processHardware;
+        public MainWindow()
+        {
+            _processHardware = new ProcessHardwareLocations.ProcessHardwareLocations();
+            InitializeComponent();
+        }
 
-       private void BtnEdit_Click(object sender, RoutedEventArgs e)
-       {
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
 
-       }
+        }
 
-       private void BtnDelete_Click(object sender, RoutedEventArgs e)
-       {
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
 
-       }
+        }
 
-       private void BtnAdd_Click(object sender, RoutedEventArgs e)
-       {
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = new Hardware
+            {
+                HardwareType = TBArt.Text,
+                HardwareName = TBName.Text,
+                BuildingName = TBBuilding.Text,
+                RoomName = TBRoom.Text
+            };
+            _processHardware.CaptureHardware(temp);
+        }
 
-       }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
 
-       private void BtnSave_Click(object sender, RoutedEventArgs e)
-       {
+        }
 
-       }
-
-       private void BtnRead_Click(object sender, RoutedEventArgs e)
-       {
-
-       }
-   }
+        private void BtnRead_Click(object sender, RoutedEventArgs e)
+        {
+            listViewHardware.DataContext = _processHardware.GetHardware();
+            listViewHardware.Items.Refresh();
+        }
+    }
 }
