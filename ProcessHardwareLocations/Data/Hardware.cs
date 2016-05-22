60219b4294ad1b3557d13e158bfbcb9c0a5e8d4a
Markus <Markus@GAMING_PC>
2016-05-22 11:33:41 +0200

    Hardware.cs: - initial commit

---
+++ ProcessHardwareLocations/Data/Hardware.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProcessHardwareLocations.Data
+{
+    public class Hardware : IHardware
+    {
+        public Guid Id { get; set; }
+        public string BuildingName { get; set; }
+        public string RoomName { get; set; }
+        public string HardwareType { get; set; }
+        public string HardwareName { get; set; }
+    }
+}
