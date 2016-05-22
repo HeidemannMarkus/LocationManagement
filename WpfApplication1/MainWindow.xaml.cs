4cad8b24ad25ba4c1778f03ea2e6f5eae769a548
bpmhell <jhoffmann@gdc.de>
2016-05-22 22:57:50 +0200

    MainWindow - Funktionen der Buttons implementiert, BtnRead in BtnLoad umbenannt

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Microsoft.Win32;
 using ProcessHardwareLocations;
 using ProcessHardwareLocations.Data;
+using System.Windows;
 
 
 namespace WpfApplication1
@@ -22,21 +11,38 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private HardwareList _hardwareList = new HardwareList();
         private IProcessHardwareLocations _processHardware;
+        private Hardware _selected;
+
         public MainWindow()
         {
             _processHardware = new ProcessHardwareLocations.ProcessHardwareLocations();
             InitializeComponent();
+            // listViewHardware.Items[0].Selected = true;
         }
 
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
+            _processHardware.DeleteHardware(_selected.Id);
 
+            var temp = new Hardware
+            {
+                HardwareType = TBArt.Text,
+                HardwareName = TBName.Text,
+                BuildingName = TBBuilding.Text,
+                RoomName = TBRoom.Text
+            };
+            _processHardware.CaptureHardware(temp);
+            listViewHardware.ItemsSource = _processHardware.GetHardware();
+            listViewHardware.Items.Refresh();
         }
 
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
         {
-
+            _processHardware.DeleteHardware(_selected.Id);
+            listViewHardware.ItemsSource = _processHardware.GetHardware();
+            listViewHardware.Items.Refresh();
         }
 
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
@@ -49,16 +55,57 @@ namespace WpfApplication1
                 RoomName = TBRoom.Text
             };
             _processHardware.CaptureHardware(temp);
+            listViewHardware.ItemsSource = _processHardware.GetHardware();
+            listViewHardware.Items.Refresh();
+
+            TBName.Clear();
+            TBArt.Clear();
+            TBBuilding.Clear();
+            TBRoom.Clear();
+
+            TBName.Focus();
         }
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
-
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = ".xml";
+            if (sfd.ShowDialog() == true)
+            {
+                _processHardware.SaveHardware(sfd.FileName);
+            }
         }
 
-        private void BtnRead_Click(object sender, RoutedEventArgs e)
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
         {
-            listViewHardware.DataContext = _processHardware.GetHardware();
+            OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.Filter = "|*.dat;*.xml;*.csv;*.json;";
+            if (ofd.ShowDialog() == true)
+            {
+                listViewHardware.ItemsSource = (HardwareList) _processHardware.LoadHardware(ofd.FileName);
+                listViewHardware.Items.Refresh();
+            }
+            TBName.Focus();
+        }
+        
+        private void listViewHardware_SelectionChanged_1(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            _selected = (Hardware) listViewHardware.SelectedItem;
+            TBName.Text = _selected.HardwareName;
+            TBArt.Text = _selected.HardwareType;
+            TBBuilding.Text = _selected.BuildingName;
+            TBRoom.Text = _selected.RoomName;
+        }
+
+        private void CBbuilding_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            listViewHardware.ItemsSource = _processHardware.GetHardware(CBbuilding.SelectedItem.ToString(), CBroom.SelectedItem.ToString());
+            listViewHardware.Items.Refresh();
+        }
+
+        private void CBroom_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            listViewHardware.ItemsSource = _processHardware.GetHardware(CBbuilding.SelectedItem.ToString(), CBroom.SelectedItem.ToString());
             listViewHardware.Items.Refresh();
         }
     }
