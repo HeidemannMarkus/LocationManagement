466c7c2b3638f3a839dbad33aff3fb984a62c70b
Markus <Markus@GAMING_PC>
2016-05-25 22:03:46 +0200

    add tests

--- ProcessHardwareLocationsBusinuessLayerTests1/ProcessHardwareLocationsTests.cs
+++ ProcessHardwareLocationsBusinuessLayerTests1/ProcessHardwareLocationsTests.cs
@@ -1,19 +1,38 @@
 ﻿using Xunit;
-using ProcessHardwareLocations;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ProcessHardwareLocations.Data;
 
 namespace ProcessHardwareLocations.Tests
 {
    public class ProcessHardwareLocationsTests
    {
       [Fact()]
-      public void SaveHardwareTest()
+      public void CaptureHardware_WithGuid_ShouldReturnError()
       {
-         Assert.True(false, "This test needs an implementation");
+         Assert.Equal(new Result
+         {
+            ErrorMessage = "Id wird intern gesetzt! Feld muss Guid.Empty enthalten",
+            HasError = true
+         }
+            , new ProcessHardwareLocations()
+            .CaptureHardware(
+               new Hardware
+               {
+                  Id = Guid.NewGuid()
+               }));
+      }
+
+      [Fact]
+      public void SaveHardware_WhenListIsEmpty_ShouldThrowError()
+      {
+         Assert.Equal(
+            new Result
+            {
+               HasError = true,
+               ErrorMessage = "Leere Liste kann nicht gespeichert werden!"
+            },
+            new ProcessHardwareLocations()
+            .SaveHardware(@"C:\test.json"));
       }
    }
 }
