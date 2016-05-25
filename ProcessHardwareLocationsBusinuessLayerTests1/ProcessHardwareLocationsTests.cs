0fcd838b2f57450fb80afc512fe17472b2190216
Markus <Markus@GAMING_PC>
2016-05-25 22:02:44 +0200

    add testproject

---
+++ ProcessHardwareLocationsBusinuessLayerTests1/ProcessHardwareLocationsTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using ProcessHardwareLocations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessHardwareLocations.Tests
+{
+   public class ProcessHardwareLocationsTests
+   {
+      [Fact()]
+      public void SaveHardwareTest()
+      {
+         Assert.True(false, "This test needs an implementation");
+      }
+   }
+}
