a6227387fe8ee6cf19572a8650821a0e0f670a93
Markus <Markus@GAMING_PC>
2016-05-25 20:41:52 +0200

    Add Testproject with first test (I)Result.cs: - Implement IEquatable as preparation for tests

--- ProcessHardwareLocations/Result.cs
+++ ProcessHardwareLocations/Result.cs
@@ -4,5 +4,9 @@
    {
       public bool HasError { get; set; }
       public string ErrorMessage { get; set; }
+      public bool Equals(IResult other)
+      {
+         return HasError == other.HasError && ErrorMessage == other.ErrorMessage;
+      }
    }
 }
---
+++ ProcessHardwareLocationsBusinuessLayerTests/ProcessHardwareLocationsTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using ProcessHardwareLocations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProcessHardwareLocations.Data;
+
+namespace ProcessHardwareLocations.Tests
+{
+   public class ProcessHardwareLocationsTests
+   {
+      [Fact()]
+      public void CaptureHardware_WithGuid_ShouldReturnError()
+      {
+         Assert.Equal(new Result
+         {
+            ErrorMessage = "Id wird intern gesetzt! Feld muss Guid.Empty enthalten",
+            HasError = true
+         }
+            , new ProcessHardwareLocations()
+            .CaptureHardware(
+               new Hardware
+               {
+                  Id = Guid.NewGuid()
+               }));
+      }
+   }
+}
